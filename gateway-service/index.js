0676a0ba197632c9fad9ff47e8c62663f02c6ebb
Alberto Martinez de Aragon Martin <108083117+AlbertoMartinezAragon@users.noreply.github.com>
2025-02-03 02:13:14 +0000

    Add files via upload

--- gateway-service/index.js
+++ gateway-service/index.js
@@ -1,1 +1,38 @@
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import { ApolloGateway, IntrospectAndCompose } from "@apollo/gateway";
 
+const gateway = new ApolloGateway({
+  supergraphSdl: new IntrospectAndCompose({
+    subgraphs: [
+      { name: "account", url: "http://account-service:4001/graphql" },
+      { name: "devices", url: "http://devices-service:4002/graphql" },
+    ],
+  }),
+});
+
+const server = new ApolloServer({
+  gateway,
+  subscriptions: false,
+  cors: {
+    origin: "http://localhost:8081", // Allow the frontend's origin
+    methods: ["GET", "POST"],
+  },
+});
+const app = express();
+
+// Root route to indicate server is working
+app.get("/", (req, res) => {
+  res.send(
+    "Apollo Server is running. Go to /graphql to interact with the API."
+  );
+});
+
+// Start the Apollo server and apply middleware
+server.start().then(() => {
+  server.applyMiddleware({ app, path: "/graphql" });
+
+  app.listen(4000, () => {
+    console.log("Server is running at http://localhost:4000/graphql");
+  });
+});
