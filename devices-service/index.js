e1ce07430769193a76c432b828058783124b97f2
Alberto Martinez de Aragon Martin <108083117+AlbertoMartinezAragon@users.noreply.github.com>
2025-02-03 02:12:24 +0000

    Add files via upload

--- devices-service/index.js
+++ devices-service/index.js
@@ -1,1 +1,48 @@
+import { ApolloServer } from "@apollo/server";
+import { gql } from "graphql-tag";
+import { buildFederatedSchema } from "@apollo/federation";
 
+const typeDefs = gql`
+  type Device @key(fields: "id") {
+    id: ID!
+    accountId: ID!
+    name: String!
+    account: Account @requires(fields: "accountId")
+  }
+
+  extend type Account @key(fields: "id") {
+    id: ID! @external
+    name: String!
+    devices: [Device]
+  }
+
+  type Query {
+    devices: [Device]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    devices: () => [
+      { id: "101", name: "iPhone 13", accountId: "1" },
+      { id: "102", name: "Samsung Galaxy", accountId: "2" },
+    ],
+  },
+  Device: {
+    account: (device) => {
+      return { id: device.accountId };
+    },
+  },
+};
+
+const server = new ApolloServer({
+  schema: buildFederatedSchema([{ typeDefs, resolvers }]),
+});
+
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+startStandaloneServer(server, {
+  listen: { port: 4002 },
+}).then(({ url }) => {
+  console.log(`Devices Service running at ${url}`);
+});
